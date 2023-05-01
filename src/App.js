678f140aeae1ef7c11769deec75d674d5f4a344a
Omer elmas <sybro6744@gmail.com>
2023-05-01 14:21:12 +0300

    elmas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { About, Country, Cv, Hero, Interests, Navbar } from './components/imports';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  const About = location.pathname === '/about';
+  const Interests = location.pathname === '/interests';
+  const Cv = location.pathname === '/cv';
+  const Country = location.pathname === '/country';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+      <Route path="/" component={<Hero/>} />
+      <Route path="/about" component={<About/>} />
+      <Route path="/interests" component={<Interests/>} />
+      <Route path="/cv" component={<Cv/>} />
+      <Route path="/country" component={<Country/>} />
+      </Routes>
+      {!About && !Interests && !Cv && !Country && (
+        <div>
+          <Navbar />
+          <Hero />
+        </div>
+      )}
     </div>
   );
 }
